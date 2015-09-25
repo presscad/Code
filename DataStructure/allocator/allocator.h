2e43ae40c5ef565fd23deea59d3dce60f75ecd17
Anh.NQV <nguyenquocvuonganh1993@gmail.com>
2015-09-25 09:51:57 +0700

    add allocator folder

---
+++ DataStructure/allocator/allocator.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  allocator.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  25/09/2015 08:02:23
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef _ALLOCATOR_H_
+#define _ALLOCATOR_H_
+
+template <typename T>
+
+
+#endif
